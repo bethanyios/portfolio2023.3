d2515fc95df85ea9d9416474d685143a146fa369
bethanyios <bethanyioscott@hotmail.co.uk>
2023-08-23 10:59:42 +0100

    add arrows to links

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import patchd from "../images/patchd.svg"
 import timeline1 from "../images/f14040.svg"
 import timeline2 from "../images/f14041.svg"
 import timeline3 from "../images/f14042.svg"
+import arrow from "../images/arrow.svg"
 
 
 import '../styling/home.css';
@@ -39,14 +40,14 @@ const Home = () => {
         </div>
         <div className="experience-container">
             <p className="experience">
-            <a href="https://www.yallacooperative.com/" target="_blank" rel="noreferrer" className="exp-title">Yalla Cooperative<br/></a> UX Designer & Researcher, Sept 2021-present</p>
+            <a href="https://www.yallacooperative.com/" target="_blank" rel="noreferrer" className="exp-title"> <img src={arrow} className="arrow" alt="link"/>Yalla Cooperative<br/></a> UX Designer & Researcher, Sept 2021-present</p>
             <p className="experience">
-            <a href="https://sienna-website.vercel.app/" target="_blank" rel="noreferrer" className="exp-title">Sienna<br/></a> Co-founder: UX Designer & Developer, Mar 2020 - Aug 2021</p>
+            <a href="https://sienna-website.vercel.app/" target="_blank" rel="noreferrer" className="exp-title"> <img src={arrow} className="arrow" alt="link"/>Sienna<br/></a> Co-founder: UX Designer & Developer, Mar 2020 - Aug 2021</p>
             <p className="experience">
 
-            <a href="https://www.foundersandcoders.com/" target="_blank" rel="noreferrer" className="exp-title">Founders & Coders <br/></a> Full-time Coding Bootcamp, Oct 2019 - Mar 2020 </p>
+            <a href="https://www.foundersandcoders.com/" target="_blank" rel="noreferrer" className="exp-title"> <img src={arrow} className="arrow" alt="link"/>Founders & Coders <br/></a> Full-time Coding Bootcamp, Oct 2019 - Mar 2020 </p>
             <p className="experience">
-            <a href="https://www.together-uk.org/" target="_blank" rel="noreferrer" className="exp-title">Together For Mental Wellbeing<br/></a> HR Administrator, Jan - Oct 2019</p>
+            <a href="https://www.together-uk.org/" target="_blank" rel="noreferrer" className="exp-title"> <img src={arrow} className="arrow" alt="link"/>Together For Mental Wellbeing<br/></a> HR Administrator, Jan - Oct 2019</p>
         </div> 
         </div>
         <div className="process-wrapper">
